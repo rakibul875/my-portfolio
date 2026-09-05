479a55dd0919551afd1c73cd4999fefa448aee76
rakibul875 <rakibulislamashik78@gmail.com>
2026-09-05 20:06:02 +0600

    implement HeroSection component with resume download functionality

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -10,8 +10,8 @@ import { Typewriter } from "react-simple-typewriter";
 export default function HeroSection() {
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/resume/Rakibul_Islam_Ashik_Resume.pdf";
-    link.download = "Rakibul_Islam_Ashik_Resume.pdf";
+    link.href = "/resume/MD_Rakibul_Islam_Ashik_full_stack_resume.pdf";
+    link.download = "MD_Rakibul_Islam_Ashik_full_stack_resume.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -41,7 +41,6 @@ export default function HeroSection() {
       id="overview"
       className="px-6 md:px-8 pt-24 md:pt-32 pb-16 md:pb-24 flex items-center relative overflow-hidden max-w-7xl mx-auto min-h-[80vh]"
     >
-  
       <motion.div
         animate={{
           scale: [1, 1.1, 1],
